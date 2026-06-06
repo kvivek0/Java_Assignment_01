51b13111bc2c15b85ad78acdd3db277bc600d7dc
kvivek0 <vivekksp963@gmail.com]>
2026-06-06 22:29:58 +0530

    Added Question05

---
+++ src/Question05.java
@@ -0,0 +1,9 @@
+//Write a program to print value of an int variable.
+
+public class Question05 {
+    static void main(String []args){
+        int x=34;
+        System.out.println(x);
+    }
+
+}
