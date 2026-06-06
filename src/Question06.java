b9a7abfe9078657a6a27bdbcdc04029d1fca1e40
kvivek0 <vivekksp963@gmail.com]>
2026-06-06 22:35:32 +0530

    Added Question06

---
+++ src/Question06.java
@@ -0,0 +1,9 @@
+//Write a program to print value of a float type variable.
+
+public class Question06 {
+    static void main(String []args){
+         float x=2.5f;
+        System.out.println(x);
+    }
+
+}
